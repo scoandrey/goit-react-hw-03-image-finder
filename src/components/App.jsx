493cde552c403e324ba056372b4c90b05634ca1a
Andrii <scoandrey@gmail.com>
2023-07-19 22:44:20 -0400

    save2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import Button from './Button/Button';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Searchbar from './Searchbar/Searchbar';
 import { Audio } from 'react-loader-spinner';
-import Modal from './Modal/Modal';
+// import Modal from './Modal/Modal';
 export const App = () => {
   return (
     <div className="App">
